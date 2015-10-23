08e0bd1a352c7b5287cafcbda2455d14d69ae059
anarkia7115 <marc7115@gmail.com>
2015-10-23 15:15:26 +0800

    smaller jar to upload

--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-aws s3 cp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/HalvadeUploaderWithLibs.jar
+mvn package
+#scp ./dependencies.jar caws:/home/gcbi/workspace/halvade_run/lib/
+scp ./target/halvade_upload-1.0-SNAPSHOT.jar caws:/home/gcbi/workspace/halvade_run/lib/
+#aws s3 cp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/HalvadeUploaderWithLibs.jar
