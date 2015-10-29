e5aa6870b6465a58ffc51105627143d5062a58be
Mark Gao <marc7115@gmail.com>
2015-10-29 17:47:02 +0800

    runnable on aws

--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-aws s3 cp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/HalvadeUploaderWithLibs.jar
+#aws s3 cp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/HalvadeUploaderWithLibs.jar
+#scp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar gcbi:/home/gcbi/workspace/halvade_run/lib/HalvadeUploaderWithLibs.jar
+scp ./target/halvade_upload-1.0-SNAPSHOT-jar-with-dependencies.jar caws:/home/gcbi/workspace/halvade_run/lib/HalvadeUploaderWithLibs.jar
+
